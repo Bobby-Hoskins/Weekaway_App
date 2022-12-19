648c2a4de134d834999f3f2745dfd3246d2d5635
Bobby-Hoskins <bobbyhoskins1986@gmail.com>
2022-12-19 11:49:54 -0500

    Created & styled topbar

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { AiFillPhone, AiOutlineClockCircle } from "react-icons/ai";
+import { TbPlane } from "react-icons/tb";
+const TopBar = () => {
+  return (
+    <div className="flex justify-between items-center px-4 py-2">
+      <div className="flex items-center">
+        <TbPlane size={30} className="text-PrimaryDark mr-2" />
+        <h1 className="font-h1Text text-xl font-bold text-gray-700">
+          WEEKAWAY
+        </h1>
+      </div>
+      <div className="flex">
+        <div className="hidden md:flex items-center px-6">
+          <AiOutlineClockCircle size={20} className="mr-2 text-PrimaryDark" />
+          <p className="text-sm text-gray-700">9AM - 5PM</p>
+        </div>
+        <div className="hidden md:flex items-center px-6">
+          <AiFillPhone size={20} className="mr-2 text-PrimaryDark" />
+          <p className="text-sm text-gray-700">1-888-815-1234</p>
+        </div>
+        <button>Get a Free Quote</button>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        PrimaryDark: "#001011",
+        PrimaryColor: "#1e75e7",
+        PrimaryAccent: "#64e9ee",
+      },
+      fontFamily: {
+        h1Text: ["Zen Dots", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
